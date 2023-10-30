89daf38361208d0ef7dbde6d4f821218d160efbc
mkwkomorowski <mkw.komorowski@gmail.com>
2023-10-30 13:22:27 +0100

    apps.py

--- blogging_for_humans/apps.py
+++ blogging_for_humans/apps.py
@@ -2,12 +2,36 @@
 blogging_for_humans Django application initialization.
 """
 
+import logging
 from django.apps import AppConfig
+from edx_django_utils.plugins import PluginURLs, PluginSettings
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
+
+
+log = logging.getLogger(__name__)
 
 
 class BloggingForHumansConfig(AppConfig):
-    """
-    Configuration for the blogging_for_humans Django application.
-    """
+    name = "my_app"
+    label = "my_app"
+    verbose_name = "My Open edX Plugin"
 
-    name = 'blogging_for_humans'
+    # See: https://edx.readthedocs.io/projects/edx-django-utils/en/latest/edx_django_utils.plugins.html
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^my-app/",
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
+        },
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: "settings.production"},
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
+            }
+        },
+    }
+    
+    def ready(self):
+        log.debug("{label} is ready.".format(label=self.label))
