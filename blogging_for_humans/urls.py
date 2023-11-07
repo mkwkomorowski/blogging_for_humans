e05725937139d477c7c6058af80e0a9593f9d9f1
developer <edxapp@plisi-dev7.novalocal>
2023-11-07 12:39:34 +0000

    test

--- blogging_for_humans/urls.py
+++ blogging_for_humans/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    # re_path(r'', TemplateView.as_view(template_name="blogging_for_humans/base.html")),
+    re_path(r'^aaa', TemplateView.as_view(template_name="blogging_for_humans/base.html")),
 ]
