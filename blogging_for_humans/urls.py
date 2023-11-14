0b7e4e4af0a066b53449c9a688eb8227312b6b90
mkwkomorowski <149168149+mkwkomorowski@users.noreply.github.com>
2023-11-14 12:56:23 +0100

    Update urls.py

--- blogging_for_humans/urls.py
+++ blogging_for_humans/urls.py
@@ -6,5 +6,8 @@ from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    re_path(r'^aaa', TemplateView.as_view(template_name="blogging_for_humans/base.html")),
+    # re_path(r'^aaa', TemplateView.as_view(template_name="blogging_for_humans/base.html")),
+    url(r"", include("lms.urls")),
+    url(r"^aaa", TemplateView.as_view(template_name="blogging_for_humans/base.html")),
+    url(r"bbb", TemplateView.as_view(template_name="blogging_for_humans/base.html")),
 ]
