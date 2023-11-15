bb5c3d13dc6003193fe73f7737e6fb4b37fe9254
mkomorowskipib <147144978+mkomorowskipib@users.noreply.github.com>
2023-11-15 09:38:36 +0100

    Update views.py

--- blogging_for_humans/views.py
+++ blogging_for_humans/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def render_something_from_model(request):
 
     a = 'blablabla'
-    query_set = models.Model_01.objects.all()
+    query_set = models.Model01.objects.all()
     for model in query_set:
         a = model
     return render(request, 'blogging_for_humans.base.html', {'name': a})
