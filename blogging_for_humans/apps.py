7ac93514725811d7b598ed4d4704eddc544d93b9
mkomorowskipib <147144978+mkomorowskipib@users.noreply.github.com>
2023-11-16 12:11:58 +0100

    Update apps.py

--- blogging_for_humans/apps.py
+++ blogging_for_humans/apps.py
@@ -9,6 +9,28 @@ from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
 
 log = logging.getLogger(__name__)
+name = "blogging_for_humans"
+label = "blogging_for_humans"
+verbose_name = "My Open edX Plugin"
+
+plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^blogging_for_humans/",
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
+        },
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: "settings.production"},
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
+                SettingsType.DEVSTACK: {PluginSettings.RELATIVE_PATH: "settings.devstack"},
+            }
+        },
+    }
+
+
 
 
 class BloggingForHumansConfig(AppConfig):
